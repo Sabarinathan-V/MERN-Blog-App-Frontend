615c278f1ccf13c35ee6d831f9053d1814dcdced
sabarinathan <sabarivanam@gmail.com>
2023-05-25 17:17:25 +0530

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,7 @@ export default function Header() {
         .catch((error) => console.log(error))
 
     }
-  }, []);
+  }, [setUserInfo, userInfo]);
 
   const logout = () => {
     // invalidate cookie
